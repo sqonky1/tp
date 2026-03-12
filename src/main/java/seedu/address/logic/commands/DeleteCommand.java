b9f0d3e7f390ade14391c2590a14ed824cc2139b
SeePui <ngseepui95@gmail.com>
2026-03-12 16:01:48 +0800

    Implement delete contact by email

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_EMAIL;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Email;
 import seedu.address.model.person.Person;
 
 /**
@@ -19,16 +21,31 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + ": Deletes the person identified by the index number or email used in the displayed person list.\n"
+            + "Parameters: i/INDEX (must be a positive integer) or e/EMAIL (must be a NUS email)\n"
+            + "Example: " + COMMAND_WORD + " i/1, " + COMMAND_WORD + " e/e1234567@u.nus.edu";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
     private final Index targetIndex;
+    private final Email targetEmail;
 
+    /**
+     * Creates a DeleteCommand using index
+     */
     public DeleteCommand(Index targetIndex) {
+        requireNonNull(targetIndex);
         this.targetIndex = targetIndex;
+        this.targetEmail = null;
+    }
+
+    /**
+     * Creates a DeleteCommand using email
+     */
+    public DeleteCommand(Email email) {
+        requireNonNull(email);
+        this.targetEmail = email;
+        this.targetIndex = null;
     }
 
     @Override
@@ -36,11 +53,18 @@ public class DeleteCommand extends Command {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        Person personToDelete;
+
+        if (targetIndex != null) {
+            if (targetIndex.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+
+            personToDelete = lastShownList.get(targetIndex.getZeroBased());
+        } else {
+            personToDelete = findPersonByEmail(lastShownList);
         }
 
-        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
         model.deletePerson(personToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
     }
@@ -57,13 +81,22 @@ public class DeleteCommand extends Command {
         }
 
         DeleteCommand otherDeleteCommand = (DeleteCommand) other;
-        return targetIndex.equals(otherDeleteCommand.targetIndex);
+        return java.util.Objects.equals(targetIndex, otherDeleteCommand.targetIndex)
+                && java.util.Objects.equals(targetEmail, otherDeleteCommand.targetEmail);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
                 .add("targetIndex", targetIndex)
+                .add("targetEmail", targetEmail)
                 .toString();
+    }
+
+    private Person findPersonByEmail(List<Person> personList) throws CommandException {
+        return personList.stream()
+                .filter(person -> person.getEmail().equals(targetEmail))
+                .findFirst()
+                .orElseThrow(() -> new CommandException(MESSAGE_INVALID_PERSON_DISPLAYED_EMAIL));
     }
 }
